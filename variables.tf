a1175913e7f3d7da26b184f53d4d8f9be0d9b311
Michael McClelland <30412622+Michael-McClelland@users.noreply.github.com>
2024-04-22 23:45:37 -0400

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,23 +1,23 @@
-variable "feature_set" {
-  description = "The feature set of the organization. One of 'ALL' or 'CONSOLIDATED_BILLING'."
-  type        = string
-  default     = "ALL"
-  nullable    = false
-}
+# variable "feature_set" {
+#   description = "The feature set of the organization. One of 'ALL' or 'CONSOLIDATED_BILLING'."
+#   type        = string
+#   default     = "ALL"
+#   nullable    = false
+# }
 
-variable "aws_service_access_principals" {
-  description = "A list of AWS service principal names for which you want to enable integration with your organization."
-  type        = list(string)
-  default     = []
-  nullable    = false
-}
+# variable "aws_service_access_principals" {
+#   description = "A list of AWS service principal names for which you want to enable integration with your organization."
+#   type        = list(string)
+#   default     = []
+#   nullable    = false
+# }
 
-variable "enabled_policy_types" {
-  description = "The list of Organizations policy types to enable in the Organization Root. The organization must have `feature_set` set to `\"ALL\"`."
-  type        = list(string)
-  default     = []
-  nullable    = false
-}
+# variable "enabled_policy_types" {
+#   description = "The list of Organizations policy types to enable in the Organization Root. The organization must have `feature_set` set to `\"ALL\"`."
+#   type        = list(string)
+#   default     = []
+#   nullable    = false
+# }
 
 variable "organization" {
   description = "The organization with the tree of organizational units and accounts to construct. Defaults to an object with an empty list of units and accounts"
